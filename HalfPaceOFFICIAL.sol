0648cb889cd262535efa545b9b70226ac2f78a9e
halfpace-admin <81193639+halfpace-admin@users.noreply.github.com>
2021-09-10 15:43:41 -0400

    Update and rename Halfpace.sol to HalfPaceOFFICIAL.sol

---
+++ HalfPaceOFFICIAL.sol
@@ -0,0 +1,121 @@
+pragma solidity ^0.4.24;
+ 
+//SafeMath Interface
+ 
+contract SafeMath {
+ 
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+ 
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+ 
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+ 
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+ 
+ 
+//ERC Token Standard #20 Interface
+ 
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+ 
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+ 
+ 
+//Contract function to receive approval and execute function in one call
+ 
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
+ 
+//PACE Token contract
+ 
+contract PACEToken is ERC20Interface, SafeMath {
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint public _totalSupply;
+    uint public _circulatingSupply;
+    uint public _liquiditySupply;
+    uint public _arbitrageSupply;
+ 
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+ 
+    constructor() public {
+        symbol = "PACE";
+        name = "PACE Token";
+        decimals = 8;
+        _totalSupply = 5200000000000000;
+        _circulatingSupply = 3200000000000000;
+        _liquiditySupply = 1000000000000000;
+        _arbitrageSupply = 1000000000000000;
+        balances[icowalletgoeshere] = _circulatingSupply; 
+        balances[liquiditywalletgoeshere] = _liquiditySupply; 
+        balances[arbitragewalletgoeshere] = _arbitrageSupply;
+        emit Transfer(address(0), icowalletgoeshere, _circulatingSupply); (address(1), liquiditywalletgoeshere, _liquiditySupply); (address(2), arbitragewalletgoeshere, _arbitrageSupply);
+    }
+ 
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply  - balances[address(0)];
+    }
+ 
+    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+        return balances[tokenOwner];
+    }
+ 
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+ 
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+ 
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+ 
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+ 
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true;
+    }
+ 
+    function () public payable {
+        revert();
+    }
+}
